71e40398578cdc6e983e3751d649f2d050100ac4
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-20 13:13:37 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,7 +1,13 @@
-  def sort_array_asc (array)
+def sort_array_asc (array)
    array.sort {|a,b| a <=> b}
-  end
-  
-  def sort_array_desc (array)
+end
+
+def sort_array_desc (array)
    array.sort {|a,b| b <=> a}
+end
+
+def sort_array_char_count (array)
+  array.sort do
+  |a,b| a <=> b && a.length <=> b.length
   end
+end
